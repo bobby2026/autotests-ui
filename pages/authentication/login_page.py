52a654486047b3166b3c30124420b2ee9f175732
bobby2026 <mark.zamark@yandex.ru>
2026-01-23 23:50:23 +0600

    Наращивание тестовой базы и рефакторинг

--- pages/authentication/login_page.py
+++ pages/authentication/login_page.py
@@ -1,4 +1,5 @@
 from playwright.sync_api import Page
+import re
 
 from components.authentication.login_form_component import LoginFormComponent
 from elements.button import Button
@@ -19,12 +20,20 @@ class LoginPage(BasePage):
             page, 'login-page-wrong-email-or-password-alert', 'Wrong email or password'
         )
 
+
     def click_login_button(self):
         self.login_button.click()
 
+
     def click_registration_link(self):
         self.registration_link.click()
+        # Добавили проверку
+        self.check_current_url(re.compile(".*/#/auth/registration"))
+
 
     def check_visible_wrong_email_or_password_alert(self):
         self.wrong_email_or_password_alert.check_visible()
         self.wrong_email_or_password_alert.check_have_text('Wrong email or password')
+
+
+
